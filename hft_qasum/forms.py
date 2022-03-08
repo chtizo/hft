615ef0b55a2240e5552e02e7005d7b08a4160868
jvrrs <jvraghavaramasharma@gmail.com>
2022-03-08 13:44:00 +0530

    some changes

--- hft_qasum/forms.py
+++ hft_qasum/forms.py
@@ -4,13 +4,13 @@ class Summarize(forms.Form):
     ins = forms.CharField()
     ins.widget.attrs.update({'name': 'ins', 'value': 'summarize', 'hidden': 'true'})
     text = forms.CharField(widget=forms.Textarea)
-    text.widget.attrs.update({'id': 'main_text', 'name':'main_text', 'rows':'15', 'cols':'60'})
+    text.widget.attrs.update({'id': 'main_text', 'name':'main_text', 'rows':'21', 'cols':'80'})
 
 class Answer(forms.Form):
     ins = forms.CharField()
     ins.widget.attrs.update({'name': 'ins', 'value': 'answer', 'hidden': 'true'})
     text = forms.CharField(widget=forms.Textarea)
-    text.widget.attrs.update({'id': 'ques', 'name':'ques', 'rows':'5', 'cols':'50'})
+    text.widget.attrs.update({'id': 'ques', 'name':'ques', 'rows':'5', 'cols':'80'})
 
 class Init(forms.Form):
     ins = forms.CharField()
